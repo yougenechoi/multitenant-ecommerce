c9ad6224279367519a0c524de407e4cedda8ba5e
yougenechoi <yougenechoi@gmail.com>
2026-01-30 20:14:25 +1300

    Update procedures.ts

--- modules/products/server/procedures.ts
+++ modules/products/server/procedures.ts
@@ -16,11 +16,13 @@ export const productsRouter = createTRPCRouter({
       const where: Where = {};
       if (input.minPrice) {
         where.price = {
+          ...where.price,
           greater_than_equal: input.minPrice,
         };
       }
       if (input.maxPrice) {
         where.price = {
+          ...where.price,
           less_than_equal: input.maxPrice,
         };
       }
