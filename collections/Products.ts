b462997fcdc302b1f6a7298a42e206d2d2a5b6f8
yougenechoi <yougenechoi@gmail.com>
2026-01-30 22:45:03 +1300

    13: API filters sorting

--- collections/Products.ts
+++ collections/Products.ts
@@ -16,6 +16,12 @@ export const Products: CollectionConfig = {
       relationTo: "categories",
       hasMany: false,
     },
+    {
+      name: "tags",
+      type: "relationship",
+      relationTo: "tags",
+      hasMany: true,
+    },
     { name: "image", type: "upload", relationTo: "media" },
     {
       name: "refundPolicy",
--- modules/products/ui/components/product-list.tsx
+++ modules/products/ui/components/product-list.tsx
@@ -2,15 +2,17 @@
 
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { useProductFilters } from "../../hooks/use-product-filters";
 
 interface Props {
   category?: string;
 }
 
 export const ProductList = ({ category }: Props) => {
+  const [filters] = useProductFilters();
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(
-    trpc.products.getMany.queryOptions({ category }),
+    trpc.products.getMany.queryOptions({ category, ...filters }),
   );
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid=cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
