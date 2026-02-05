074482489c7b1344aa3d4ae1e1a712232dc65008
yougenechoi <yougenechoi@gmail.com>
2026-02-05 21:41:59 +1300

    19: checkout page

--- trpc/routers/_app.ts
+++ trpc/routers/_app.ts
@@ -4,12 +4,14 @@ import { categoriesRouter } from "@/modules/categories/server/procedures";
 import { productsRouter } from "@/modules/products/server/procedures";
 import { tagsRouter } from "@/modules/tags/server/procedures";
 import { tenantsRouter } from "@/modules/tenants/server/procedures";
+import { checkoutRouter } from "@/modules/checkout/server/procedures";
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   categories: categoriesRouter,
   products: productsRouter,
   tags: tagsRouter,
   tenants: tenantsRouter,
+  checkout: checkoutRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
