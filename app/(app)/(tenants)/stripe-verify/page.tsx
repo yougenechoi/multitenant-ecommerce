4f34080ee6dc20697c2eb5c4a818eb99a69f46c5
yougenechoi <yougenechoi@gmail.com>
2026-02-07 12:11:36 +1300

    25: stripe connect

---
+++ app/(app)/(tenants)/stripe-verify/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useTRPC } from "@/trpc/client"; // ✅ Correct import
+import { useMutation } from "@tanstack/react-query";
+import { LoaderIcon } from "lucide-react";
+import { useEffect } from "react";
+
+const Page = () => {
+  const trpc = useTRPC();
+  const { mutate: verify } = useMutation(
+    trpc.checkout.verify.mutationOptions({
+      onSuccess: (data) => {
+        window.location.href = data.url;
+      },
+      onError: () => {
+        window.location.href = "/";
+      },
+    }),
+  );
+
+  useEffect(() => {
+    verify();
+  }, [verify]);
+
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <LoaderIcon className="animate-spin text-muted-foreground" />
+    </div>
+  );
+};
+
+export default Page;
