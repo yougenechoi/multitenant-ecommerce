3a292d562a9fd4223e4ac20583d338ccdce98a3e
yougenechoi <yougenechoi@gmail.com>
2026-02-06 19:16:05 +1300

    22: reviews

--- modules/library/server/procedures.ts
+++ modules/library/server/procedures.ts
@@ -1,14 +1,54 @@
 import { Media, Tenant } from "@/payload-types";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import z from "zod";
-import { DEFAULT_TAG_LIMIT } from "@/constant";
+import { DEFAULT_LIMIT } from "@/constant";
+import { TRPCError } from "@trpc/server";
 
 export const libraryRouter = createTRPCRouter({
+  getOne: protectedProcedure
+    .input(
+      z.object({
+        productId: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const ordersData = await ctx.payload.find({
+        collection: "orders",
+        limit: 1,
+        pagination: false,
+        where: {
+          and: [
+            { product: { equals: input.productId } },
+            { user: { equals: ctx.session.user.id } },
+          ],
+        },
+      });
+
+      const order = ordersData.docs[0];
+
+      if (!order) {
+        throw new TRPCError({ code: "NOT_FOUND", message: "Order not found" });
+      }
+
+      const product = await ctx.payload.findByID({
+        collection: "products",
+        id: input.productId,
+      });
+
+      if (!product) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Product not found",
+        });
+      }
+
+      return product;
+    }),
   getMany: protectedProcedure
     .input(
       z.object({
         cursor: z.number().default(1),
-        limit: z.number().default(DEFAULT_TAG_LIMIT),
+        limit: z.number().default(DEFAULT_LIMIT),
       }),
     )
     .query(async ({ ctx, input }) => {
