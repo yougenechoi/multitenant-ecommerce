0f1be29c68784a9397effa0fbe8146e7d2d8fdeb
yougenechoi <yougenechoi@gmail.com>
2026-02-08 15:46:53 +1300

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,9 +7,8 @@ export function cn(...inputs: ClassValue[]) {
 
 export function generateTenantURL(tenantSlug: string) {
   const isDevelopment = process.env.NODE_ENV === "development";
-  const isSubdomainRoutingEnabled = Boolean(
-    process.env.NEXT_PUBLIC_ENABLE_SUBDOMAIN_ROUTING!,
-  );
+  const isSubdomainRoutingEnabled =
+    process.env.NEXT_PUBLIC_ENABLE_SUBDOMAIN_ROUTING === "true";
 
   // In development or subdomain routing disabled mode, use normal routing
   if (isDevelopment || !isSubdomainRoutingEnabled) {
